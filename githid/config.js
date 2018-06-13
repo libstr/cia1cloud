d035e7e3bc501b1504f0fdcd53e11f517805ac7a
libstr <libinjoseph18@gmail.com>
2018-06-13 19:11:28 +0530

    config

---
+++ githid/config.js
@@ -0,0 +1,8 @@
+  var config = {
+    apiKey: "AIzaSyA_f9oQ7RLJyGKHHbCKAlpVsHjKQcBFGrw",
+    authDomain: "cia1-cloud.firebaseapp.com",
+    databaseURL: "https://cia1-cloud.firebaseio.com",
+    projectId: "cia1-cloud",
+    storageBucket: "cia1-cloud.appspot.com",
+    messagingSenderId: "244753486937"
+  };
